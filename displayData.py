ed8cd7488bc3a8704f1469f73f12a4f4451ab456
Jose  Iturria <jiturria@bmicos.com>
2022-02-25 14:00:01 -0500

    added bar chart, fixed some columns

--- displayData.py
+++ displayData.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import altair as alt
 
 # streamlit run displayData.py 
 st.set_page_config( page_title="Churn Detection Modeling",
@@ -8,21 +9,12 @@ st.set_page_config( page_title="Churn Detection Modeling",
 
 df = pd.read_csv('combined.csv')
 
-churn_df = df
-
-#churn_drop = ["Account ID",	"Full Account ID",	"Full User ID",	"Owner AMA / AUM",
-	# "Connect 2021 from Opty	Connect 2021"	,"Connect 2020 from Opty",	"Connect 2020",
-    # 	"Connect 2019",	"JUNO Account ID",	"Kaseya Market Segment",	"Auto Renewal Op Out",
-    #     	"# of Closed Won Opportunities",	"Account Record Type",	"Type",	"Last Activity",
-    #         	"Last Modified Date",	"Min of Effective_Start_Date__c",	"Max of Effective_End_Date__c",	"Max of Effective_Start_Date__c",
-    #             	"Current State",	"account_user_id",	"reputation_to_date	created_at", "closed_at",
-    #                 	"min_IO_seats_required",	"region"]
-#churn_df.drop(columns=churn_drop, inplace=True)
+df.drop(columns="Unnamed: 0", inplace=True)
 
 st.header("Combined data set:")
 st.selectbox("Charts", ["Churn model", "Churn factors"])
 st.dataframe(df)
-#st.bar_chart(churn_df)
+st.bar_chart(df['Churn'].value_counts())
 
 # SIDEBAR
 st.sidebar.header("Filters:")
@@ -31,7 +23,7 @@ region = st.sidebar.multiselect(
     options=df['region'].unique(),
     default=df['region'].unique()
 )
-st.sidebar.caption("The regions consists of North America(NA), APAC(AU), and Europe(EU)")
+st.sidebar.caption("The regions consists of North America(NAM), APAC(AU), and Europe(EU)")
 ownerType = st.sidebar.multiselect(
     "Select the owner type:",
     options=df['Owner AMA / AUM'].unique(),
