40d9818714e04a6e4c3e02be17f92caf963d4955
airruti <54050924+airruti@users.noreply.github.com>
2022-03-14 14:12:01 -0400

    cleaned csvs

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 
-
 #Read CSV files
 product_data_df = pd.read_csv("JUNO - Cleaned Product Data - FINAL.csv")
 subscription_df = pd.read_csv("JUNO - Subscriptions.csv")
@@ -13,20 +12,25 @@ combined_df = temp_df.merge(product_data_df, left_on='JUNO Account ID', right_on
 
 #This will fill the "region" column not sure why it messes up 
 #when we do the merge... but we might want to use region for our model
-combined_df['region'].loc[combined_df['Account Currency'] == 'USD'] = "NAM"
-combined_df['region'].loc[combined_df['Account Currency'] == 'CAD'] = "NAM"
-combined_df['region'].loc[combined_df['Account Currency'] == 'EUR'] = "EU"
-combined_df['region'].loc[combined_df['Account Currency'] == 'GBP'] = "EU"
-combined_df['region'].loc[combined_df['Account Currency'] == 'AUD'] = "AU"
-combined_df['region'].loc[combined_df['Account Currency'] == 'NZD'] = "AU"
-combined_df['region'].loc[combined_df['Account Currency'] == 'ZAR'] = "SA"
+account_currency_map = {'USD': 'NAM', 'CAD': 'NAM', 'EUR': 'EU', 'GBP': 'EU', 'AUD': 'AU', 'NZD': 'AU', 'ZAR': 'SA'}
+combined_df['Account Currency'] = combined_df['Account Currency'].map(account_currency_map)
 
-#Drop repeating columns/stuff we don't need
+
+#csv for diplaydata for UI
 combined_df.to_csv("display.csv")
 
-drop = ["Account ID", "JUNO Account ID", "Current State", "created_at", "Last Activity", "Last Modified Date", "closed_at", "Min of Effective_Start_Date__c", "Owner AMA / AUM", "Max of Effective_End_Date__c", "Account Currency", "account_id", "account_user_id", "Row Labels", "Full Account ID", "Max of Effective_Start_Date__c", "min_IO_seats_required", "Full User ID", "Account Record Type", "Type"]
+##todo: 
+"""
+Calculate Churn within Python
+Data cleaning
+Add dates to Churn for future dates 
+"""
+
+#Drop repeating columns/stuff we don't need
+drop = ["Account ID", "Full User ID", "JUNO Account ID", "Row Labels", "Full Account ID",  "account_id", "account_user_id", "Current State", "created_at", "Last Activity", "Last Modified Date", "closed_at", "Min of Effective_Start_Date__c", "Owner AMA / AUM", "Max of Effective_End_Date__c", "Account Currency",  "Max of Effective_Start_Date__c", "min_IO_seats_required",  "Account Record Type", "Type"]
 combined_df.drop(columns=drop, inplace=True)
 
+#make indicator variables
 combined_df = pd.get_dummies(combined_df, columns=["Kaseya Market Segment", "region"])
 
 combined_df = combined_df[combined_df['Duration'] != '#NUM!']
